982e10251a3a37ec68e8600acc9f268f6899c4b9
Tuumix <wellz-tkn@hotmail.com>
2020-05-10 15:29:53 -0300

    precisa arrumar o estilo do textfield

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { Button, TextField, withStyles} from '@material-ui/core';
+import * as XLSX from 'xlsx';
+import styles from './App.css';
 
-function App() {
+const CssTextField = withStyles({
+  root: {
+    '& label.Mui-focused': {
+      color: '#2ecc71',
+    },
+    '& label.color': {
+      color: '#2ecc71',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#2ecc71',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'red',
+      },
+      '&:hover fieldset': {
+        borderColor: 'yellow',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'green',
+      },
+    },
+  },
+})(TextField);
+
+const cor = theme => ({
+  input: {
+      color: "pink"
+  }
+});
+
+
+
+function App(props) {
+  const { classes } = props;
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+      const bstr = reader.result;
+      const wb = XLSX.read(bstr, {type:'binary'});
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      const data = XLSX.utils.sheet_to_csv(ws, {header:1});
+      console.log("Data>>>"+data);
+      }
+      reader.readAsBinaryString(file)
+    })
+    
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="div_esquerda">
+        <div>
+          <CssTextField label="teste" InputProps={{className: classes.input,}} />
+        </div>
+      </div>
+      <div className="div_direita">
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+      </div>
     </div>
   );
 }
